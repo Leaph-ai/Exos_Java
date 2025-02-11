425e6e9d66a69c8eeebe91a03325eb384205b772
Philippe Boudegna <philippe.boudegna@gmail.com>
2025-02-11 14:20:24 +0100

    exercices 1 2 3

---
+++ Methods/src/Addition.java
@@ -0,0 +1,10 @@
+class Addition {
+    public int additionner(int a, int b) {
+        return a + b;
+    }
+
+    public double additionner(double a, double b) {
+        return a + b;
+    }
+}
+
---
+++ Methods/src/Exo1.java
@@ -0,0 +1,7 @@
+public class Exo1 {
+    public static void main(String[] args) {
+        Addition a = new Addition();
+        System.out.println(a.additionner(10, 20));
+        System.out.println(a.additionner(10.5, 20.5));
+    }
+}
