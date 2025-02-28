a3c2bd789eb204e0aed6ce08af1b2766a5dad3f1
Philippe Boudegna <philippe.boudegna@gmail.com>
2025-02-28 11:25:44 +0100

    exo 1 finished

--- JavaFX_1ersExo/Exo1/src/Main.java
+++ JavaFX_1ersExo/Exo1/src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Cliquez-moi");
+        btn.setOnAction(e -> System.out.println("Bonjour, JavaFX!"));
+
+        Scene scene = new Scene(btn, 300, 200);
+        primaryStage.setTitle("Hello World JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
